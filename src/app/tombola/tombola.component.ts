8c7613a0473adc9a6420620d1c0087ca2b10ba03
John  Lema Yungan <johnlemayungan@MacBook-Pro-de-John.local>
2024-02-26 17:39:12 -0500

    Inicio

---
+++ src/app/tombola/tombola.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tombola',
+  templateUrl: './tombola.component.html',
+  styleUrl: './tombola.component.scss'
+})
+export class TombolaComponent {
+
+}
